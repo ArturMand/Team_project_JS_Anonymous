b1dff5366146978b68ef73714a68990d473e10e7
EugeneDemchenko <108480374+EugeneDemchenko@users.noreply.github.com>
2022-10-11 13:04:59 +0300

    alphabet

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import { getCocktailsByLetter, getCocktailByWord, getRandomCocktail } from "./js/cocktailsAPI";
 import { cardBuilder, randomCardBuilder } from "./js/cocktailCard";
 // import { Notify } from 'notiflix/build/notiflix-notify-aio';
-// import { addHeroSearch } from "./js/heroAlphabet";
+import { addHeroSearch } from "./js/heroAlphabet";
 import { mobileMenu } from './js/mobile-menu';
 import { findCocktailBySearch } from "./js/searchCocktail";
 import debounce from 'lodash.debounce';
-
+import { refs } from './js/selectorRefs';
 
 
 mobileMenu();
@@ -17,6 +17,7 @@ getRandomCocktail()
 const searchForm = document.querySelector('#form__search');
 searchForm.addEventListener('input', debounce(findCocktailBySearch, 500))
 
-
+const letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0',];
+addHeroSearch(letters)
 
 
