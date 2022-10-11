a38404199d3c7d50d0d472672369f144a1b6acc3
EugeneDemchenko <108480374+EugeneDemchenko@users.noreply.github.com>
2022-10-11 16:12:44 +0300

    Update searchCocktail.js

--- src/js/searchCocktail.js
+++ src/js/searchCocktail.js
@@ -1,5 +1,6 @@
 import { getCocktailsByLetter, getCocktailByWord, getRandomCocktail} from "./cocktailsAPI";
 import { cardBuilder, randomCardBuilder} from "./cocktailCard";
+import { Notify } from "notiflix";
 
 const cocktailList = document.querySelector('.cocktails__list');
 
@@ -8,7 +9,8 @@ export function findCocktailBySearch(e) {
     e.preventDefault()
     const { searchQuery } = e.target.elements
     const formValue = searchQuery.value.trim().toLowerCase()
-    console.log(formValue);
+    // console.log(e);
+
     if (formValue.length === 0) {
         // console.log(e.target.value.length);
         cocktailList.innerHTML = '';
@@ -25,7 +27,10 @@ export function findCocktailBySearch(e) {
     else {
         getCocktailByWord(formValue)
         .then(cardBuilder)
-        .catch(error => { console.log(error); })
+            .catch(error => {
+                Notify.failure(`you must die!`)
+                // console.log(error);
+            })
     }
    
 }
