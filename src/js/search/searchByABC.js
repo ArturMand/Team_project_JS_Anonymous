269ddf690da37e5b2b0e430a305b208ddf090619
EugeneDemchenko <108480374+EugeneDemchenko@users.noreply.github.com>
2022-10-12 23:51:01 +0300

    fixErrorMsg

--- src/js/search/searchByABC.js
+++ src/js/search/searchByABC.js
@@ -1,8 +1,6 @@
 import { getCocktailsByLetter } from '../fetch/fetchCardByLetter';
 import { cardBuilder } from '../builders/cardBuilder';
-import { Notify } from 'notiflix/build/notiflix-notify-aio';
-import { refs } from '../refs/selectorRefs';
-import { errorMarkup } from '../markups/errorMarkup';
+
 
 export function searchByABC(e) {
   if (e.target.tagName !== 'BUTTON') return;
@@ -10,8 +8,6 @@ export function searchByABC(e) {
   getCocktailsByLetter(ourLetter)
     .then(cardBuilder)
     .catch(error => {
-      Notify.failure(`We haven't cocktail, which begin with that symbol`);
-      refs.gallery.innerHTML = errorMarkup()
       console.log(error);
     });
 }
