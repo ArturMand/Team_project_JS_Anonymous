f0e38725213bff0a0cc192f65d7b70757ead9f7b
Yuliia Kulish <kulish.yulechka@gmail.com>
2022-10-13 21:38:59 +0300

    Update removeFromFav.js

--- src/js/favorite-page/removeFromFav.js
+++ src/js/favorite-page/removeFromFav.js
@@ -2,12 +2,16 @@ import {refs} from "../refs/selectorRefs";
 import { saveCocktail } from '../localStorage/getId';
 import { pageFavCocktails } from "./favCocktailsPage";
 
-export function removeFav() {
+export function removeFav(e) {
     refs.listFavCock.addEventListener('click', removeFromFavorite);
 
     function removeFromFavorite(e) {
+        if (!e.target.dataset.favorite) {
+        return;
+    }
         saveCocktail(e);
         refs.listFavCock.innerHTML = '';
         pageFavCocktails();
     };
+    
 }
