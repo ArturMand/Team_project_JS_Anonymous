b6b7a5ddfd5b34f1de431846f0b135fb4330c55c
ShcherbatayaLuna <105154403+ShcherbatayaLuna@users.noreply.github.com>
2022-10-13 15:42:11 +0300

    Update files

---
+++ src/favorite.js
@@ -0,0 +1,13 @@
+import { startPageBuild } from './js/builders/startPageBuild';
+import { addHeroSearch } from './js/builders/heroAlphabet';
+import { mobileMenu } from './js/mobile-menu/mobile-menu';
+import { findCocktailBySearch } from './js/search/searchCocktail';
+import { refs } from './js/refs/selectorRefs';
+import { searchByABC } from './js/search/searchByABC';
+import { openModal } from './js/modal/modal';
+import { Navigation } from './js/nav-link/navigation';
+
+mobileMenu();
+
+refs.searchForm.addEventListener('submit', findCocktailBySearch);
+refs.gallery.addEventListener('click', openModal);
